283a9705a5bef141b685aa7e049db1b1afb0283c
jjo3ys <jjo3ys@gmail.com>
2021-11-03 15:46:30 +0900

    <feat> basic settings update

---
+++ config/my_settings.py
@@ -0,0 +1,16 @@
+
+import os 
+
+EMAIL = {
+'EMAIL_BACKEND' : 'django.core.mail.backends.smtp.EmailBackend',
+'EMAIL_USE_TLS' : True,
+'EMAIL_PORT' : 587,
+'EMAIL_HOST' : 'smtp.gmail.com',   
+'EMAIL_HOST_USER' : 'loopus2021@gmail.com',                    
+'EMAIL_HOST_PASSWORD' : 'loopus483',
+# 'REDIRECT_PAGE' : 'http://10.58.5.40:3000/signin'
+}
+S3 = {
+    'access_key':'AKIA4XCDN5N2IUMDHTQG',
+    'secret_key':'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'
+}
