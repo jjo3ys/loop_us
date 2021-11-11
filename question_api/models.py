3fbe7586783f72a166339def24b2044149776b3d
gilhan-yong <rlfgks97@gmail.com>
2021-11-11 16:07:34 +0900

    <feat> model Question initialized

--- question_api/models.py
+++ question_api/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+
+class Question(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    content = models.TextField(null=True)
+    adopt = models.BooleanField(null=False)
+    date = models.DateField(null=True)
+
+
+class Answer(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    content = models.TextField(null=True)
+    adopt = models.BooleanField(null=False)
+    date = models.DateField(null=True)
+
