c3532668d32c3ca61da91ca246b117feac416523
jjo3ys <jjo3ys@gmail.com>
2021-11-17 12:12:51 +0900

    <feat> tagging on create & update

--- project_api/serializers.py
+++ project_api/serializers.py
@@ -1,11 +1,21 @@
+from tag.models import Project_Tag
 from .models import Project
 from rest_framework import serializers
 from post_api.serializers import PostingSerializer
 
+class ProjectTagSerializer(serializers.ModelSerializer):
+    tag = serializers.SerializerMethodField()
+    class Meta:
+        model = Project_Tag
+        fields = ['tag_id', 'tag']
+    
+    def get_tag(self, obj):
+        return obj.tag.tag
 
 class ProjectSerializer(serializers.ModelSerializer):
     post = PostingSerializer(many=True, read_only=True)
+    project_tag = ProjectTagSerializer(many=True, read_only=True)
 
     class Meta:
         model = Project
-        fields = ['id', 'project_name', 'introduction', 'start_date', 'end_date', 'post']
+        fields = ['id', 'project_name', 'introduction', 'start_date', 'end_date', 'post', 'project_tag']
--- project_api/views.py
+++ project_api/views.py
@@ -1,5 +1,6 @@
-from .serializers import ProjectSerializer
+from .serializers import ProjectSerializer, ProjectTagSerializer
 from .models import Project
+from tag.models import Tag, Project_Tag
 
 from django.contrib.auth.models import User
 from rest_framework import status
@@ -24,10 +25,70 @@ def create_project(request):
                                          start_date = start_date,
                                          end_date = end_date)
 
-    project_sz = ProjectSerializer(project_obj)
+    tag_list = request.data['tag']
+    for tag in tag_list:
+        try:
+            tag_obj = Tag.objects.get(tag=tag)
+            tag_obj.count = tag_obj.count + 1
+            tag_obj.save()
 
+        except Tag.DoesNotExist:
+            tag_obj = Tag.objects.create(tag = tag)
+        
+        Project_Tag.objects.create(project=project_obj, tag=tag_obj)
+
+    project_sz = ProjectSerializer(project_obj)
     return Response(project_sz.data, status=status.HTTP_201_CREATED)
 
+@api_view(['POST', ])
+@permission_classes((IsAuthenticated,))
+def update_project(request, idx):
+    start_date = request.data['start_date']
+    end_date = request.data['end_date']
+    tag_list = request.data['tag']
+
+    if request.data['end_date'] == '':
+        end_date = None
+        
+    project_obj = Project.objects.get(id=idx)
+    project_obj.project_name = request.data['project_name']
+    project_obj.introduction = request.data['introduction']
+    project_obj.start_date = start_date
+    project_obj.end_date = end_date
+    project_obj.save()
+
+    old_tag = Project_Tag.objects.filter(project=idx)
+    old_sz = ProjectTagSerializer(old_tag, many=True)
+    old_tag = old_sz.data
+    old_list = []
+
+    for tag in old_tag:
+        old_list.append(tag['tag'])
+
+        if tag['tag'] not in tag_list:
+            Project_Tag.objects.get(tag_id = tag['tag_id'], project = project_obj).delete()
+            tag_obj = Tag.objects.get(id=tag['tag_id'])
+            tag_obj.count = tag_obj.count - 1
+            if tag_obj.count == 0:
+                tag_obj.delete()
+                
+            tag_obj.save()
+
+    for tag in tag_list:
+        if tag in old_list:
+            continue
+        else: 
+            try:
+                tag_obj = Tag.objects.get(tag=tag)
+                tag_obj.count = tag_obj.count + 1
+                tag_obj.save()
+
+            except Tag.DoesNotExist:
+                tag_obj = Tag.objects.create(tag = tag)
+
+            Project_Tag.objects.create(tag = tag_obj, project = project_obj)
+
+    return Response("ok")
 @api_view(['GET', ])
 @permission_classes((IsAuthenticated,))
 def load_project(request, idx):
@@ -37,6 +98,7 @@ def load_project(request, idx):
     project_obj = Project.objects.filter(user=idx)
 
     project_sz = ProjectSerializer(project_obj, many=True)  
+
     return_dict.update({'project':project_sz.data})
 
     if str(user.id) == idx:
