5ae9b167ea8c1149f35097a66142513214651f72
jjo3ys <jjo3ys@gmail.com>
2021-12-23 13:45:40 +0900

    <feat> post load with project info

--- project_api/serializers.py
+++ project_api/serializers.py
@@ -2,7 +2,6 @@ from re import search
 from tag.models import Project_Tag
 from .models import Project, TagLooper
 from rest_framework import serializers
-from post_api.serializers import PostingSerializer
 from user_api.models import Profile
 from user_api.serializers import SimpleProfileSerializer
 
@@ -25,11 +24,17 @@ class ProjectLooperSerializer(serializers.ModelSerializer):
         profile = Profile.objects.get(user_id=obj.looper.id)
         return SimpleProfileSerializer(profile).data
 
+class SimpleProjectserializer(serializers.ModelSerializer):
+    project_tag = ProjectTagSerializer(many=True, read_only=True)
+    
+    class Meta:
+        model = Project
+        fields =['id', 'project_name', 'project_tag']
+        
 class ProjectSerializer(serializers.ModelSerializer):
-    post = PostingSerializer(many=True, read_only=True)
     project_tag = ProjectTagSerializer(many=True, read_only=True)
     looper = ProjectLooperSerializer(many=True, read_only=True)
 
     class Meta:
         model = Project
-        fields = ['id', 'project_name', 'introduction', 'start_date', 'end_date', 'post', 'project_tag', 'looper']
+        fields = ['id', 'project_name', 'introduction', 'start_date', 'end_date', 'project_tag', 'looper']
