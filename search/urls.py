23db16133be3b0ff7ea5db81916afff5bd1a1f70
jjo3ys <jjo3ys@gmail.com>
2021-12-29 16:36:59 +0900

    <fix> log with page 1

--- search/urls.py
+++ search/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('search_log/<type>/', views.log),
     path('search/<type>/', views.search)
 ]
--- search/views.py
+++ search/views.py
@@ -22,72 +22,11 @@ from tag.models import Project_Tag
 
 @api_view(['GET', ])
 @permission_classes((IsAuthenticated,))
-def log(request, type):
-    user_id = request.user.id
-    query = request.GET['query']
-    Log.objects.create(user_id=user_id, query=query)
-
-    if type == 'post':
-        obj = Post.objects.filter(Q(contents__icontains=query)|Q(title__icontains=query)).order_by('-id')
-        obj = Paginator(obj, 5).get_page(1)
-        data_list = PostingSerializer(obj, many=True).data
-        for i in range(5):
-            profile = Profile.objects.get(user=obj[i].user)
-            data_list[i].update(SimpleProjectserializer(obj[i].project).data)
-            data_list[i].update(SimpleProfileSerializer(profile).data)
-        return Response(data_list, status=status.HTTP_200_OK)
-
-    elif type == 'project':
-        obj = Project.objects.filter(Q(project_name__icontains=query)|Q(introduction__icontains=query)).order_by('-id')
-        obj = Paginator(obj, 5).get_page(1)
-        data_list = ProjectSerializer(obj, many=True).data
-        for i in range(5):
-            profile = Profile.objects.get(user=obj[i].user)
-            data_list[i].update(SimpleProfileSerializer(profile).data)
-        return Response(data_list, status=status.HTTP_200_OK)  
-
-    elif type == 'profile':
-        obj = Profile.objects.filter(real_name__icontains=query).order_by('-id')
-        obj = Paginator(obj, 5).get_page(1)
-        return Response(ProfileSerializer(obj, many=True).data, status=status.HTTP_200_OK)  
-
-    elif type == 'question':
-        obj = Question.objects.filter(content__icontains=query).order_by('-id')
-        obj = Paginator(obj, 5).get_page(1)
-        data_list = QuestionSerializer(obj, many=True).data
-        for i in range(5):
-            profile = Profile.objects.get(user=obj[i].user)
-            data_list[i].update(SimpleProfileSerializer(profile).data)
-        return Response(data_list, status=status.HTTP_200_OK)  
-
-    elif type == 'tag':
-        obj = Project_Tag.objects.filter(tag_id=int(query))
-        obj = Paginator(obj, 5).get_page(1)
-        result = []
-        for o in obj:
-            if o.project not in result:
-                result.append(o.project)
-        result.reverse()
-        data_list = ProjectSerializer(result, many=True).data
-        for i in range(len(result)):
-            try:
-                profile = Profile.objects.get(user_id=data_list[i]['user_id'])
-                data_list[i].update(SimpleProfileSerializer(profile).data)
-            except Profile.DoesNotExist:
-                data_list[i].update({"real_name":"DoesNotExist",
-                                     "profile_image":None,
-                                     "department":"DoesNotExist"})
-        return Response(data_list, status=status.HTTP_200_OK)
-
-
-    elif type == 'notice':
-        return Response("unrealized", status=status.HTTP_204_NO_CONTENT)
-
-@api_view(['GET', ])
-@permission_classes((IsAuthenticated,))
 def search(request, type):
     query = request.GET['query']
     page = request.GET['page']
+    if page == 1:
+        Log.objects.create(user_id=request.user.id, query=query)
 
     if type == 'post':
         obj = Post.objects.filter(Q(contents__icontains=query)|Q(title__icontains=query)).order_by('-id')
