319f490c72f7df3409cd48e7651d13ec2f34af48
jjo3ys <jjo3ys@gmail.com>
2021-12-31 13:22:06 +0900

    <feat> corporate model

--- user_api/models.py
+++ user_api/models.py
@@ -1,8 +1,5 @@
 from django.db import models
 from django.conf import settings
-from .department import DEPARTMENT
-# Create your models here.
-DEPARTMENT_CHOICES = (DEPARTMENT.items())
 
 class Profile(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, unique=True)
@@ -10,4 +7,13 @@ class Profile(models.Model):
     type = models.SmallIntegerField(default=0)
     profile_image = models.ImageField(null = True)
     department = models.CharField(default='기타', max_length=15)
+
+    class Meta:
+        db_table = "profile"
+
+class Company_Inform(models.Model):
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    corp_num = models.CharField(max_length=10)
+    corp_name = models.TextField()
+    tel_num = models.CharField(max_length=13)
 
--- user_api/tests.py
+++ user_api/tests.py
@@ -1,14 +1,17 @@
-from django.conf.global_settings import DATE_INPUT_FORMATS
 import requests
 import json
+import zipfile
+from io import BytesIO
 
-key = 'frbSEAcXD%2Ba6UEOUq1lkWcWmFoDku20SZvLeO%2B%2BpP9e5yQo5GIqTkbVbctqafewScJuzLLcTW%2Fl4d%2FLw%2FwjOig%3D%3D'
-data = {   
-    "b_no":[
-        "2208162517"
-    ]
-}
-params = {}
-# data = json.dumps(data)
-res = requests.post('http://api.odcloud.kr/api/nts-businessman/v1/status?serviceKey=frbSEAcXD%2Ba6UEOUq1lkWcWmFoDku20SZvLeO%2B%2BpP9e5yQo5GIqTkbVbctqafewScJuzLLcTW%2Fl4d%2FLw%2FwjOig%3D%3D', data=data)
-print(res.json())
+# key = 'frbSEAcXD%2Ba6UEOUq1lkWcWmFoDku20SZvLeO%2B%2BpP9e5yQo5GIqTkbVbctqafewScJuzLLcTW%2Fl4d%2FLw%2FwjOig%3D%3D'
+# data = {   
+#     "b_no":[
+#         "2208162517"
+#     ]
+# }
+#5240901930
+# params = {}
+# # data = json.dumps(data)
+# res = requests.post('http://api.odcloud.kr/api/nts-businessman/v1/status?serviceKey=frbSEAcXD%2Ba6UEOUq1lkWcWmFoDku20SZvLeO%2B%2BpP9e5yQo5GIqTkbVbctqafewScJuzLLcTW%2Fl4d%2FLw%2FwjOig%3D%3D', data=data)
+# print(res.json())
+key = 'b189feeceeeb10c0512d9fb3cf7fd32bf53095b5'
