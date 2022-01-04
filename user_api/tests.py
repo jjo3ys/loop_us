e179ea346f68e934f1672b00892aa282a6d4f695
jjo3ys <jjo3ys@gmail.com>
2022-01-04 15:26:25 +0900

    <fix> user to user_id

--- user_api/tests.py
+++ user_api/tests.py
@@ -1,17 +1,18 @@
 import requests
-import json
-import zipfile
-from io import BytesIO
+import pprint
 
-# key = 'frbSEAcXD%2Ba6UEOUq1lkWcWmFoDku20SZvLeO%2B%2BpP9e5yQo5GIqTkbVbctqafewScJuzLLcTW%2Fl4d%2FLw%2FwjOig%3D%3D'
-# data = {   
-#     "b_no":[
-#         "2208162517"
-#     ]
-# }
-#5240901930
-# params = {}
-# # data = json.dumps(data)
-# res = requests.post('http://api.odcloud.kr/api/nts-businessman/v1/status?serviceKey=frbSEAcXD%2Ba6UEOUq1lkWcWmFoDku20SZvLeO%2B%2BpP9e5yQo5GIqTkbVbctqafewScJuzLLcTW%2Fl4d%2FLw%2FwjOig%3D%3D', data=data)
-# print(res.json())
-key = 'b189feeceeeb10c0512d9fb3cf7fd32bf53095b5'
+headers = {
+    'Accpet':'application/json',
+    'Authorization':'frbSEAcXD+a6UEOUq1lkWcWmFoDku20SZvLeO++pP9e5yQo5GIqTkbVbctqafewScJuzLLcTW/l4d/Lw/wjOig==',
+    'Content-Type': 'application/json',
+}
+
+params = (
+    ('serviceKey', 'frbSEAcXD+a6UEOUq1lkWcWmFoDku20SZvLeO++pP9e5yQo5GIqTkbVbctqafewScJuzLLcTW/l4d/Lw/wjOig=='),
+)
+
+data = {"b_no":['2208162517']}
+
+response = requests.post('http://api.odcloud.kr/api/nts-businessman/v1/status', headers=headers, params=params, data=data)
+
+print(response.json())
