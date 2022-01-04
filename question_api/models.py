b984931175181ea4e564816de1d406da106ac9a0
jjo3ys <jjo3ys@gmail.com>
2022-01-04 12:12:43 +0900

    <feat> p2p question

--- question_api/models.py
+++ question_api/models.py
@@ -11,8 +11,7 @@ class Question(models.Model):
     date = models.DateField(auto_now_add=True)
 
     class Meta:
-        db_table = "Q_question"
-
+        db_table = "Question"
 
 class Answer(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
@@ -23,3 +22,23 @@ class Answer(models.Model):
 
     class Meta:
         db_table = "Q_answer"
+
+class P2PQuestion(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='user_question', on_delete=models.CASCADE)
+    to = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='to_question', on_delete=models.CASCADE)
+    content = models.TextField(null=True)
+    adopt = models.BooleanField(null=False)
+    date = models.DateField(auto_now_add=True)
+
+    class Meta:
+        db_table = "Q_p2p_q"    
+
+class P2PAnswer(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    question = models.ForeignKey(P2PQuestion, related_name='p2panswer', on_delete=models.CASCADE)
+    content = models.TextField(null=True)
+    adopt = models.BooleanField(null=False)
+    date = models.DateField(auto_now_add=True)
+
+    class Meta:
+        db_table = "Q_p2p_a"
