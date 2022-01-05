5edf24fb35b1b373c80a0e9a76913e20b66a5a1c
jjo3ys <jjo3ys@gmail.com>
2022-01-05 15:25:49 +0900

    <fix> loop_request url

--- loop/urls.py
+++ loop/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('loop_request/<idx>', views.loop),
+    path('loop_request/<idx>', views.loop_request),
     path('loop/<idx>', views.loop),
     path('unloop/<idx>', views.unloop),
     path('get_list/<idx>', views.get_list),
