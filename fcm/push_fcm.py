9378a60c9ba94bacc56ff5f5fedcbec74c314c52
jjo3ys <jjo3ys@gmail.com>
2022-01-05 15:25:02 +0900

    <feat> alarm

--- fcm/push_fcm.py
+++ fcm/push_fcm.py
@@ -1,3 +1,11 @@
 from firebase_admin import messaging
 
-def send_fcm():
+def send_fcm(token, req_from):
+    message = messaging.Message(notification=messaging.Notification(
+        title='루프요청',
+        body='{0}님이 루프요청 하셨습니다'.format(req_from)
+    ),
+    token = token,
+    # topic='37'
+    )
+    messaging.send(message)
