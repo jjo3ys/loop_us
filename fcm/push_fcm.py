91f2f712d6d0305f72f4c0452d20b2c29e83edea
jjo3ys <jjo3ys@gmail.com>
2022-01-08 21:37:32 +0900

    <feat> send msg with alarm

--- fcm/push_fcm.py
+++ fcm/push_fcm.py
@@ -1,6 +1,6 @@
 from firebase_admin import messaging
 
-def send_fcm(token, req_from):
+def loop_fcm(token, req_from):
     message = messaging.Message(notification=messaging.Notification(
         title='루프요청',
         body='{0}님이 루프요청 하셨습니다'.format(req_from)
@@ -9,3 +9,22 @@ def send_fcm(token, req_from):
     # topic='37'
     )
     messaging.send(message)
+
+def chat_fcm(token, req_from, msg):
+    message = messaging.Message(notification=messaging.Notification(
+        title='{0}님이 메세지를 남겼습니다.'.format(req_from),
+        body=msg
+    ),
+    token=token
+    )
+    messaging.send(message)
+
+def notification_fcm(token_list):
+    text = input("메세지:")
+    msg = messaging.MulticastMessage(notification=messaging.Notification(
+        title='공지',
+        body=text
+    ),
+    tokens=token_list
+    )
+    messaging.send_multicast(msg)
