ccd8d4f3c7ba7b5cecc814d7e2a2039a12983f34
jjo3ys <jjo3ys@gmail.com>
2022-01-11 12:59:04 +0900

    <feat> multi database with chatting model

--- chat/models.py
+++ chat/models.py
@@ -4,14 +4,18 @@ from django.contrib.auth.models import User
 class Room(models.Model):
     member = models.JSONField(default={})
     class Meta:
+        app_label = "chatting"
         db_table = 'Msg_room'
 
 class Msg(models.Model):
     room = models.ForeignKey(Room, on_delete=models.CASCADE)
-    receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name='receiver')
+    receiver_id = models.IntegerField()
     message = models.CharField(max_length=1200)
     date = models.DateTimeField(auto_now_add=True)
     is_read = models.BooleanField(default=False)
 
     class Meta:
+        app_label = "chatting"
         db_table = 'Msg'
+
+#chat model migrate 할 때 python manage.py migrate chat --database=chatting
--- chat/views.py
+++ chat/views.py
@@ -10,46 +10,35 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
-from django.db.models import Q
 
 @api_view(['POST', 'GET'])
 @permission_classes((IsAuthenticated,))
 def chatting(request, receiver_idx):
     user = request.user
-    if request.method == 'GET':
-        member_list=[user.id, int(receiver_idx)]
-        member_list.sort()
-        
+    member_list=[user.id, int(receiver_idx)]    
+    member_list.sort()
+    if request.method == 'GET':     
         room = Room.objects.get(member=member_list)
         message_obj = Msg.objects.filter(room_id=room.id)
 
         for msg in message_obj:
-            if msg.receiver_id == user.id:
+            if msg.receiver_id == user.id and not msg.is_read:
                 msg.is_read = True
                 msg.save()
 
         message = ChatSerializer(message_obj, many=True).data
-        return_dict = {"messages":message}
-        profile = Profile.objects.get(user_id=receiver_idx)
-        profile = SimpleProfileSerializer(profile).data
-        return_dict.update(profile)
 
-        return Response(return_dict, status=status.HTTP_200_OK)
+        return Response(message, status=status.HTTP_200_OK)
 
     if request.method == 'POST':
-        member_list=[user.id, int(receiver_idx)]
-        member_list.sort()
-
-        room = Room.objects.get_or_create(member=member_list)
-        msg = Msg.objects.create(room_id=room[0].id,
+        room = Room.objects.get_or_create(member=member_list)[0]
+        msg = Msg.objects.create(room_id=room.id,
                                  receiver_id=receiver_idx,
                                  message=request.data['message'],
                                  is_read=False)
                     
         message = ChatSerializer(msg).data
-        profile = Profile.objects.get(user_id=receiver_idx)
-        profile = SimpleProfileSerializer(profile).data
-        message.update(profile)
+
         try:
             send_profile = Profile.objects.get(user_id=user.id) 
             receiver_token = FcmToken.objects.get(user_id=receiver_idx)    
