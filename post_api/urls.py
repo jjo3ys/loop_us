34113692d4c80deed1e1ad81f075cabfa9a442cd
jjo3ys <jjo3ys@gmail.com>
2022-01-11 15:25:52 +0900

    <fix>url

--- post_api/urls.py
+++ post_api/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('posting_upload/<proj_idx>/', views.posting_upload, name='posting_upload'),
-    path('specific_posting_load/<posting_idx>/', views.specific_posting_load, name='specific_posting_load'),
+    path('specific_posting_load/<posting_idx>', views.specific_posting_load, name='specific_posting_load'),
     path('specific_posting_update/<posting_idx>/', views.specific_posting_update, name='specific_posting_update'),
     path('like/<idx>/', views.like, name='like'),
     path('like_list_load/<idx>/', views.like_list_load),
--- project_api/urls.py
+++ project_api/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('create_project/', views.create_project),
     path('update_project/<idx>/', views.update_project),
-    path('load_project/<idx>/', views.load_project),
+    path('load_project/<idx>', views.load_project),
     path('delete_project/<idx>/', views.delete_project)
 ]
