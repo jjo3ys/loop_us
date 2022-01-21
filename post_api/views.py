69dbdcb084e3fa9ec3ba70c9461e97139d37a6c5
jjo3ys <jjo3ys@gmail.com>
2022-01-21 17:23:15 +0900

    <feat>recommend list load

--- post_api/views.py
+++ post_api/views.py
@@ -1,10 +1,11 @@
+import re
 from django.core.paginator import Paginator
 
-from tag.models import Project_Tag
+from tag.models import Profile_Tag, Project_Tag
 from fcm.models import FcmToken
 from fcm.push_fcm import like_fcm
 from user_api.models import Profile
-from user_api.serializers import SimpleProfileSerializer as ProfileSerializer
+from user_api.serializers import SimpleProfileSerializer
 
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.decorators import api_view, permission_classes
@@ -18,41 +19,102 @@ from loop.models import Loopship
 
 import random
 # Create your views here.
-@api_view(['POST', ])
+@api_view(['POST', 'PUT', 'GET', 'DELETE'])
 @permission_classes((IsAuthenticated,))
-def posting_upload(request, proj_idx):
-    post_obj = Post.objects.create(user_id=request.user.id, 
-                                   project_id=proj_idx,
-                                   title=request.data['title'],
-                                   thumbnail=request.FILES.get('thumbnail'))
+def posting(request):
+    if request.method == 'POST':           
+        post_obj = Post.objects.create(user_id=request.user.id, 
+                                    project_id=request.GET['id'],
+                                    title=request.data['title'],
+                                    thumbnail=request.FILES.get('thumbnail'))
 
 
-    for image in request.FILES.getlist('image'):
-        ContentsImage.objects.create(post_id=post_obj.id,
-                                     image=image)
+        for image in request.FILES.getlist('image'):
+            ContentsImage.objects.create(post_id=post_obj.id,
+                                        image=image)
 
-    images = PostingContentsImageSerializer(ContentsImage.objects.filter(post_id=post_obj.id), many=True).data
-    image_id = 0
-    contents = []
+        images = PostingContentsImageSerializer(ContentsImage.objects.filter(post_id=post_obj.id), many=True).data
+        image_id = 0
+        contents = []
 
-    for d in eval(request.data['contents']):
-        if d['content'] == 'image':
-            d['content'] = images[image_id]['image']
-            image_id += 1
+        for d in eval(request.data['contents']):
+            if d['content'] == 'image':
+                d['content'] = images[image_id]['image']
+                image_id += 1
 
-        contents.append(d)
+            contents.append(d)
 
-    post_obj.contents = str(contents)
-    post_obj.save()
+        post_obj.contents = str(contents)
+        post_obj.save()
 
-    return Response("ok", status=status.HTTP_200_OK)
+        return Response("ok", status=status.HTTP_200_OK)
+    
+    elif request.method == 'PUT':
+        post_obj = Post.objects.get(id=request.GET['id'])
+        post_obj.title = request.data['title']
+        post_obj.thumbnail = request.FILES.get('thumbnail')
+    
+    elif request.method == 'GET':
+        user_id = request.user.id
+        posting_idx = request.GET['id']
 
-@api_view(['POST', ])
-@permission_classes((IsAuthenticated,))
-def specific_posting_update(request, posting_idx):
-    post_obj = Post.objects.get(id=posting_idx)
-    post_obj.title = request.data['title']
-    post_obj.thumbnail = request.FILES.get('thumbnail')
+        try:
+            post_obj = Post.objects.get(id=posting_idx)
+
+        except Post.DoesNotExist:
+            return Response('The posting isn\'t valid', status=status.HTTP_404_NOT_FOUND)
+
+        postingSZ = PostingSerializer(post_obj)
+
+        profile_obj = Profile.objects.get(user_id=post_obj.user_id)
+        profile = SimpleProfileSerializer(profile_obj).data
+        project = SimpleProjectserializer(post_obj.project).data
+        return_dict = {
+            'posting_info': postingSZ.data,
+        }
+        return_dict.update(profile)
+        return_dict.update(project)
+
+        tag_list = []
+        for tag in project['project_tag']:
+            tag_list.append(int(tag['tag_id']))
+
+        recommend = Project_Tag.objects.filter(tag_id__in=tag_list)
+        recommend_post = []
+
+        for pj_tag in recommend:
+            post = Post.objects.filter(project=pj_tag.project)
+            for p in post:
+                if p.id != post_obj.id and p not in recommend_post:
+                    recommend_post.append(p)
+
+        recommend_post = random.sample(recommend_post, min(3, len(recommend_post)))
+        
+        recommend_post = MainloadSerializer(recommend_post, many=True).data
+        return_dict.update({"recommend_post":recommend_post})
+
+        if user_id == post_obj.user_id:
+            return_dict.update({"is_user":1})
+        else:
+            return_dict.update({"is_user":0})
+
+        try:
+            Like.objects.get(user_id=request.user.id, post_id=posting_idx)
+            return_dict.update({"is_liked":1})
+        except:
+            return_dict.update({"is_liked":0})
+
+        try:
+            BookMark.objects.get(user_id=request.user.id, post_id=posting_idx)
+            return_dict.update({"is_marked":1})
+        except:
+            return_dict.update({"is_marked":0})
+        return Response(return_dict)
+    
+    elif request.method == 'DELETE':
+        post_obj = Post.objects.get(id=request.GET['id'])
+        post_obj.delete()
+        return Response("delete posting", status=status.HTTP_200_OK)
 
 @api_view(['POST', ])
 @permission_classes((IsAuthenticated,))
@@ -94,9 +156,7 @@ def bookmark_list_load(request):
     post = MainloadSerializer(post_obj, many=True).data
 
     for i in range(len(post_obj)):
-        profile_obj = Profile.objects.get(user=post_obj[i].user)
-        post[i].update(SimpleProjectserializer(post_obj[i].project).data)    
-        post[i].update(ProfileSerializer(profile_obj).data)
+        post[i].update(SimpleProfileSerializer(Profile.objects.get(user=post_obj[i].user)).data)
         try:
             Like.objects.get(user_id=request.user.id, post_id=post_obj[i].id)
             post[i].update({"is_liked":1})
@@ -109,6 +169,46 @@ def bookmark_list_load(request):
 
 @api_view(['GET', ])
 @permission_classes((IsAuthenticated,))
+def recommend_load(request):
+    profile = Profile.objects.get(user_id=request.user.id)
+    tags = Profile_Tag.objects.filter(profile_id=profile.id)
+    tag_list = []
+    for tag in tags:
+        tag_list.append(tag.tag.id)
+
+    projects = Project_Tag.objects.filter(tag_id__in=tag_list)
+    project_list = []
+    for project in projects:
+        if project.project.user_id != request.user.id:
+            project_list.append(project.project.id)
+    
+    if request.GET['last'] == '0':
+        post_obj = list(Post.objects.filter(project_id__in = project_list))[-5:]
+    else:
+        post_obj = list(Post.objects.filter(project_id__in = project_list, id__lt=request.GET['last']))[-5:]
+    
+    post_obj.reverse()
+    post = MainloadSerializer(post_obj, many=True).data
+    for i in range(len(post_obj)):
+        post[i].update(SimpleProfileSerializer(Profile.objects.get(user=post_obj[i].user)).data)
+        post[i].update({"is_user":0})
+
+        try:
+            Like.objects.get(user_id=request.user.id, post_id=post_obj[i].id)
+            post[i].update({"is_liked":1})
+        except:
+            post[i].update({"is_liked":0})
+
+        try:
+            BookMark.objects.get(user_id=request.user.id, post_id=post_obj[i].id)
+            post[i].update({"is_marked":1})
+        except:
+            post[i].update({"is_marked":0})
+
+    return Response(post, status=status.HTTP_200_OK)
+    
+@api_view(['GET', ])
+@permission_classes((IsAuthenticated,))
 def main_load(request):
     if request.GET['last'] == '0':
         post_obj = list(Post.objects.all())[-5:]
@@ -119,9 +219,7 @@ def main_load(request):
     post = MainloadSerializer(post_obj, many=True).data
 
     for i in range(len(post_obj)):
-        profile_obj = Profile.objects.get(user=post_obj[i].user)
-        post[i].update(SimpleProjectserializer(post_obj[i].project).data)    
-        post[i].update(ProfileSerializer(profile_obj).data)
+        post[i].update(SimpleProfileSerializer(Profile.objects.get(user=post_obj[i].user)).data)
         if post_obj[i].user.id == request.user.id:
             post[i].update({"is_user":1})
         else:
@@ -158,9 +256,7 @@ def loop_load(request):
     post_obj.reverse()
     post = MainloadSerializer(post_obj, many=True).data
     for i in range(len(post_obj)):
-        profile_obj = Profile.objects.get(user=post_obj[i].user)
-        post[i].update(SimpleProjectserializer(post_obj[i].project).data)    
-        post[i].update(ProfileSerializer(profile_obj).data)
+        post[i].update(SimpleProfileSerializer(Profile.objects.get(user=post_obj[i].user)).data)
         if post_obj[i].user.id == request.user.id:
             post[i].update({"is_user":1})
         else:
@@ -180,64 +276,6 @@ def loop_load(request):
 
     return Response(post, status=status.HTTP_200_OK)
 
-
-@api_view(['GET', ])
-@permission_classes((IsAuthenticated,))
-def specific_posting_load(request, posting_idx):
-    user_id = request.user.id
-    try:
-        post_obj = Post.objects.get(id=posting_idx)
-
-    except Post.DoesNotExist:
-        return Response('The posting isn\'t valid', status=status.HTTP_404_NOT_FOUND)
-
-    postingSZ = PostingSerializer(post_obj)
-
-    profile_obj = Profile.objects.get(user_id=post_obj.user_id)
-    profile = ProfileSerializer(profile_obj).data
-    project = SimpleProjectserializer(post_obj.project).data
-    return_dict = {
-        'posting_info': postingSZ.data,
-    }
-    return_dict.update(profile)
-    return_dict.update(project)
-
-    tag_list = []
-    for tag in project['project_tag']:
-        tag_list.append(int(tag['tag_id']))
-
-    recommend = Project_Tag.objects.filter(tag_id__in=tag_list)
-    recommend_post = []
-
-    for pj_tag in recommend:
-        post = Post.objects.filter(project=pj_tag.project)
-        for p in post:
-            if p.id != post_obj.id and p not in recommend_post:
-                recommend_post.append(p)
-
-    recommend_post = random.sample(recommend_post, min(3, len(recommend_post)))
-    
-    recommend_post = MainloadSerializer(recommend_post, many=True).data
-    return_dict.update({"recommend_post":recommend_post})
-
-    if user_id == post_obj.user_id:
-        return_dict.update({"is_user":1})
-    else:
-        return_dict.update({"is_user":0})
-
-    try:
-        Like.objects.get(user_id=request.user.id, post_id=posting_idx)
-        return_dict.update({"is_liked":1})
-    except:
-        return_dict.update({"is_liked":0})
-
-    try:
-        BookMark.objects.get(user_id=request.user.id, post_id=posting_idx)
-        return_dict.update({"is_marked":1})
-    except:
-        return_dict.update({"is_marked":0})
-    return Response(return_dict)
-
 @api_view(['GET', ])
 @permission_classes((IsAuthenticated,))
 def like_list_load(request, idx):
@@ -246,12 +284,4 @@ def like_list_load(request, idx):
     for l in like_obj:
        like_list.append(Profile.objects.get(user_id=l.user_id))
     
-    return Response(ProfileSerializer(like_list, many=True).data, status=status.HTTP_200_OK)
-        
-
-@api_view(['POST', ])
-@permission_classes((IsAuthenticated,))
-def posting_delete(request, idx):
-    post_obj = Post.objects.get(id=idx)
-    post_obj.delete()
-    return Response("delete posting", status=status.HTTP_200_OK)
+    return Response(SimpleProfileSerializer(like_list, many=True).data, status=status.HTTP_200_OK)
