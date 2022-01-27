d2b91998c2e4ed770e279206d1ce4f58186b6292
jjo3ys <jjo3ys@gmail.com>
2022-01-27 11:41:48 +0900

    <fix> return results using serializer

--- tag/views.py
+++ tag/views.py
@@ -6,25 +6,14 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
 # Create your views here.
-@api_view(['POST', 'GET'])
+@api_view(['GET'])
 def tag(request):
-    if request.method == 'POST':
-        tag_sz = TagSerializer(data={'tag':request.data['tag']})
-        if tag_sz.is_valid():
-            tag_sz.save()
-        
-        return Response({'tag':tag_sz.data}, status=status.HTTP_201_CREATED)
 
-    elif request.method == 'GET':
-        return_dict = {}
-        return_dict['results'] = []
+    return_dict = {}
+    return_dict['results'] = []
 
-        tags = Tag.objects.filter(tag__icontains=request.GET['query']).order_by('-count')
-        tags = tags[:10]
+    tags = Tag.objects.filter(tag__icontains=request.GET['query']).order_by('-count')
+    tags = tags[:10]
+    return_dict['results'] = TagSerializer(tags, many=True).data
 
-        for tag in tags:
-            return_dict['results'].append({'id':tag.id,
-                                        'tag':tag.tag,
-                                        'count':tag.count})
-
-        return Response(return_dict, status=status.HTTP_200_OK)
+    return Response(return_dict, status=status.HTTP_200_OK)
