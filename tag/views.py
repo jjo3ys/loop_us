e5c4c1ea04eead9675e61010824b5da594a6c69d
jjo3ys <jjo3ys@gmail.com>
2022-01-28 13:17:14 +0900

    <feat> exactly query rank 1

--- tag/views.py
+++ tag/views.py
@@ -8,12 +8,19 @@ from rest_framework.decorators import api_view
 # Create your views here.
 @api_view(['GET'])
 def tag(request):
-
     return_dict = {}
-    return_dict['results'] = []
+    try:
+        tag = Tag.objects.get(tag=request.GET['query'])
+        result_list = [tag]
+    except:
+        result_list = []
 
     tags = Tag.objects.filter(tag__icontains=request.GET['query']).order_by('-count')
     tags = tags[:10]
-    return_dict['results'] = TagSerializer(tags, many=True).data
-
+    
+    for tag in tags:
+        if tag not in result_list:
+            result_list.append(tag)
+            
+    return_dict['results'] = TagSerializer(result_list, many=True).data
     return Response(return_dict, status=status.HTTP_200_OK)
