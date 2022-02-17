c4906803c739e5a6fce3c49194c722f197979a8a
jjo3ys <jjo3ys@gmail.com>
2022-02-17 15:54:37 +0900

    <fix>plus chat method

--- chat/views.py
+++ chat/views.py
@@ -11,7 +11,7 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 
-@api_view(['POST', 'GET'])
+@api_view(['POST', 'GET', 'PUT'])
 @permission_classes((IsAuthenticated,))
 def chatting(request):
     user = request.user
