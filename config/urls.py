2773fbdac53089b1d05d8745e30b0ebd29c3c9a4
jjo3ys <jjo3ys@naver.com>
2022-02-22 14:05:03 +0900

    <fix> static url patterns

--- config/urls.py
+++ config/urls.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
@@ -19,4 +20,4 @@ urlpatterns = [
     path('search_api/', include('search.urls')),
     path('alarm/', include('fcm.urls')),
     path('chat/', include('chat.urls')),
-]+ static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+]+ staticfiles_urlpatterns()
