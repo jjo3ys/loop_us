790d78385fbe67467dc51245989f0816c04e6f03
jjo3ys <jjo3ys@gmail.com>
2022-03-11 17:11:54 +0900

    <fix> check_token try except

--- user_api/views.py
+++ user_api/views.py
@@ -261,11 +261,14 @@ def login(request):
 @api_view(['POST'])
 @permission_classes((IsAuthenticated,))
 def check_token(request):
-    if request.data['fcm_token'] != FcmToken.objects.get(user_id=request.user.id).token:
+    try:
+        if request.data['fcm_token'] != FcmToken.objects.get(user_id=request.user.id).token:
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
+        else:
+            return Response(status=status.HTTP_200_OK)
+    except:
         return Response(status=status.HTTP_401_UNAUTHORIZED)
-    else:
-        return Response(status=status.HTTP_200_OK)
-
+        
 @api_view(['POST'])
 @permission_classes((IsAuthenticated,))
 def logout(request):
