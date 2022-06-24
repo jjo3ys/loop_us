580ad766de81b283fd9a6625520ccd39150fc08c
jjo3ys <jjo3ys@gmail.com>
2022-06-24 13:16:47 +0900

    <feat>real time hot posting

--- rank_api/models.py
+++ rank_api/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from post_api.models import Post
 
 # Create your models here.
+class PostingRanking(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='ranked_post')
+    group = models.PositiveSmallIntegerField(default=10)
+    score = models.SmallIntegerField(default=0)
+
+    class Meta:
+        db_table = 'PostingRanking'
