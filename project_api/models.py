07dd057580e9f8c554b7d88d73b2120b5cb70f39
jjo3ys <jjo3ys@gmail.com>
2022-09-19 14:42:44 +0900

    <fix> thumbnail on project

--- project_api/models.py
+++ project_api/models.py
@@ -6,7 +6,7 @@ class Project(models.Model):
     project_name = models.CharField(max_length=50)
     post_update_date = models.DateTimeField(null=True, default=None)
     group = models.PositiveSmallIntegerField(default=10)
-    thumbnail = models.PositiveBigIntegerField(default=None)
+    thumbnail = models.PositiveBigIntegerField(null=True, default=0)
     is_public = models.BooleanField(default=False)
     class Meta:
         db_table = 'Project'
