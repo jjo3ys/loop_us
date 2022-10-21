f7b29773c3c8b8dca8fa2af5c13c6eebe3859e83
jjo3ys <jjo3ys@gmail.com>
2022-10-21 11:23:46 +0900

    <fix>check certify num in activate function

--- user_api/views.py
+++ user_api/views.py
@@ -98,11 +98,8 @@ def activate(request):
     email = request.data['email']
     certify_num = request.data['certify_num']
     r = client.get(email.replace('@', ''))
-    print(r)
-    print(type(r))
-    print(certify_num)
-    print(type(certify_num))
-    if r == certify_num:
+
+    if str(r) == certify_num:
         client.delete(email)
         user = User.objects.filter(username=email['id'])
         if user.exists():
