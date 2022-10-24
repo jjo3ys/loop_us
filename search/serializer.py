ce54b6985492ce15dba200de281400631ee43f6b
jjo3ys <jjo3ys@gmail.com>
2022-10-24 16:26:22 +0900

    <feat> search long

--- search/serializer.py
+++ search/serializer.py
@@ -2,17 +2,19 @@ from rest_framework import serializers
 
 from .models import Log
 
-from user_api.models import Profile
-from user_api.serializers import SimpleProfileSerializer
+from user_api.models import Profile, Company_Inform
+from user_api.serializers import SimpleProfileSerializer, SimpleComapnyProfileSerializer
 
 class LogSerializer(serializers.ModelSerializer):
     data = serializers.SerializerMethodField()
     class Meta:
         model = Log
-        fields = ['type', 'data']
+        fields = ['id', 'type', 'data']
     
     def get_data(self, obj):
-        if obj.type == 1:
+        if obj.type == 0:
             return SimpleProfileSerializer(Profile.objects.filter(user_id=int(obj.query))).data
+        elif obj.type == 3:
+            return SimpleComapnyProfileSerializer(Company_Inform.objects.filter(user_id=int(obj.query))).data
         else:
             return obj.query
--- search/urls.py
+++ search/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     # path('connect', views.connection),
     # path('connect_log', views.connection_log),
     path('search/<type>', views.search),
+    path('search_log', views.search_log),
     path('search_uni', views.search_university),
     path('recommend', views.recommend),
     path('search_company', views.search_company),
