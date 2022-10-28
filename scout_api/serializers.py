43742d252ef4fe0988c706916a860d30717c5ae0
geun-hyoung <90395564+geun-hyoung@users.noreply.github.com>
2022-10-28 16:39:03 +0900

    <changes>scout_api to simple and rearrange

--- scout_api/serializers.py
+++ scout_api/serializers.py
@@ -1,3 +1,4 @@
+from tag.models import Group
 from user_api.models import Company, Company_Inform, CompanyImage
 from rest_framework import serializers
 
@@ -24,3 +25,13 @@ class CompanyProfileSerializer(serializers.ModelSerializer):
     
     def get_company_images(self, obj):
         return CompanyImageSerializer(CompanyImage.objects.filter(company_info_id = obj.id).first()).data
+
+class GroupSerializers(serializers.ModelSerializer):
+    companies = serializers.SerializerMethodField()
+    
+    class Meta:
+        model = Group
+        fields = ['id', 'companies']
+    
+    def get_companies(self, obj):
+        return CompanyProfileSerializer(Company_Inform.objects.filter(group = obj.id)[:3], many = True).data
--- scout_api/views.py
+++ scout_api/views.py
@@ -6,7 +6,8 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.decorators import api_view, permission_classes
 from loop.models import Loopship
 
-from scout_api.serializers import CompanyProfileSerializer
+from scout_api.serializers import CompanyProfileSerializer, GroupSerializers
+from tag.models import Group
 
 from user_api.models import Company_Inform, Profile
 
@@ -20,33 +21,57 @@ def recommendation_company(request):
 @permission_classes((IsAuthenticated,))
 def company_group(request):
     try:
-        if request.GET['type'] == 'all':
-            company_obj = Company_Inform.objects.filter(company_name__icontains=request.GET['query']).order_by('-category')
+        if request.GET['type'] == 'main':
         
+            group_obj = Group.objects.all()
+            group_obj = Paginator(group_obj, 3)
+
+            if group_obj.num_pages < int(request.GET['page']):
+
+                return Response(status=status.HTTP_204_NO_CONTENT)
+            
+            small_group = GroupSerializers(group_obj.get_page(request.GET['page']), many=True).data
+
+            for group in small_group:
+                for company in group['companies']:
+
+                    follow = Loopship.objects.filter(user_id=request.user.id, friend_id=company['user']).exists()
+                    following = Loopship.objects.filter(user_id=company['user'], friend_id=request.user.id).exists()
+
+                    if follow and following:
+                        company.update({'looped':3})
+                    elif follow:
+                        company.update({'looped':2})
+                    elif following:
+                        company.update({'looped':1})
+                    else:
+                        company.update({'looped':0})
+
+            return Response(small_group, status=status.HTTP_200_OK)
+
         else:
-            company_obj = Company_Inform.objects.filter(company_name__icontains=request.GET['query'], group = request.GET['type']).order_by('-id')
+            company_obj = Company_Inform.objects.filter(group = request.GET['type']).order_by('-id')
+            company_obj = Paginator(company_obj, 10)
+            if company_obj.num_pages < int(request.GET['page']):
+                return Response(status=status.HTTP_204_NO_CONTENT)
 
-        company_obj = Paginator(company_obj, 10)
+            companies = CompanyProfileSerializer(company_obj.get_page(request.GET['page']), many=True).data
+            for company in companies:
 
-        if company_obj.num_pages < int(request.GET['page']):
-            return Response(status=status.HTTP_204_NO_CONTENT)
+                follow = Loopship.objects.filter(user_id=request.user.id, friend_id=company['user']).exists()
+                following = Loopship.objects.filter(user_id=company['user'], friend_id=request.user.id).exists()
 
-        companies = CompanyProfileSerializer(company_obj.get_page(request.GET['page']), many=True).data
-        for company in companies:
-
-            follow = Loopship.objects.filter(user_id=request.user.id, friend_id=company['user']).exists()
-            following = Loopship.objects.filter(user_id=company['user'], friend_id=request.user.id).exists()
-
-            if follow and following:
-                company.update({'looped':3})
-            elif follow:
-                company.update({'looped':2})
-            elif following:
-                company.update({'looped':1})
-            else:
-                company.update({'looped':0})
+                if follow and following:
+                    company.update({'looped':3})
+                elif follow:
+                    company.update({'looped':2})
+                elif following:
+                    company.update({'looped':1})
+                else:
+                    company.update({'looped':0})
 
         return Response(companies, status=status.HTTP_200_OK)
     except:
         return Response(status=status.HTTP_204_NO_CONTENT)
+
 
