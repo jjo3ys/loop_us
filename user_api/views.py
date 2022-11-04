d300455b1608d5aee1e432dddd4d5f03451e1ddb
jjo3ys <jjo3ys@gmail.com>
2022-11-04 17:02:59 +0900

    <fix> alarm read with alarm id

--- user_api/views.py
+++ user_api/views.py
@@ -696,10 +696,7 @@ def ban(request):
 def alarm(request):
     if request.method == 'GET':
         if request.GET['type'] == 'read':
-            alarm_obj = Alarm.objects.filter(target_id=request.GET['id'], type=request.GET['type_id'], alarm_from_id=request.GET['sender_id'])[0]
-            alarm_obj.is_read = True
-            alarm_obj.save()
-
+            Alarm.objects.filter(id=request.GET['id']).update(is_read=True)
             return Response(status=status.HTTP_200_OK)
         
         else:         
