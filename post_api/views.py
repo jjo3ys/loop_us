346290aa45c4815cb4b62b6a1954dbfb32205600
jjo3ys <jjo3ys@gmail.com>
2022-12-12 13:30:21 +0900

    <fix>picked loopuscontents

--- post_api/views.py
+++ post_api/views.py
@@ -531,8 +531,8 @@ def main_load(request):
         ban_list += Profile.objects.filter(type=1).values_list('user_id', flat=True)
         
     if request.GET['last'] == '0':
-        post_obj = list(Post.objects.filter(user_id=1038).order_by('-id'))
-        post_obj += list(Post.objects.all().select_related('user', 'project').prefetch_related('contents_image', 'contents_link', 'contents_file', 'comments__cocomments', 'post_like', 'post_tag').exclude(user_id__in=ban_list).exclude(user_id=1038).order_by('-id')[:20])
+        post_obj = Post.objects.filter(user_id=1038).order_by('-id')
+        post_obj += Post.objects.all().select_related('user', 'project').prefetch_related('contents_image', 'contents_link', 'contents_file', 'comments__cocomments', 'post_like', 'post_tag').exclude(user_id__in=ban_list).exclude(user_id=1038).order_by('-id')[:20]
         
     else:
         post_obj = Post.objects.filter(id__lt=request.GET['last']).select_related('user', 'project').prefetch_related('contents_image', 'contents_link', 'contents_file', 'comments__cocomments', 'post_like', 'post_tag').exclude(user_id__in=ban_list).order_by('-id')[:20]
