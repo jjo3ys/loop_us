be131fb8ad96951611c8a2dd950133686f42e583
jjo3ys <jjo3ys@gmail.com>
2023-02-08 22:02:13 +0900

    <feat> prefetch list & select related list in Post model

--- career/utils.py
+++ career/utils.py
@@ -10,3 +10,6 @@ def delete_tag(tag_obj):
                 pass
         else:
             tag.tag.save()
+
+POST_PREFETCH_LIST = ["contents_image", "contents_link", "contents_file", "comments__cocomments", "post_like", "post_tag"]
+POST_SELECTE_LIST = ["career"]
