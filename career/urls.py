78060c12ed3d525c543ba4768b50e77d97ce58fd
jjo3ys <jjo3ys@gmail.com>
2023-02-10 20:29:09 +0900

    <feat> api urls

--- career/urls.py
+++ career/urls.py
@@ -3,4 +3,10 @@ from .views import *
 
 urlpatterns = [
     path("tagged_post", TagPost.as_view()),
+    path("post", PostAPI.as_view()),
+    path("career", CareerAPI.as_view()),
+    path("main", MainPageAPI.as_view()),
+    path("comment", CommentAPI.as_view()),
+    path("like", LikeAPI.as_view()),
+    path("bookmark", BookMarkAPI.as_view())
 ]
